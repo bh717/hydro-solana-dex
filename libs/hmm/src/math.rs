f7cd1460164b1e94f9792c01407d8d8ba126529d
WebDev717 <lasse.lin717@gmail.com>
2021-12-08 20:22:43 +1100

    import HMM math library into this monorepo

---
+++ libs/hmm/src/math.rs
@@ -0,0 +1,179 @@
+//! Math functions
+
+use spl_math::precise_number::PreciseNumber;
+
+/// Return the number of bits necessary to represent the integer s in binary excluding
+/// the sign and leading zeroes.
+fn bit_length(s: u128) -> u32 {
+    let mut high = 128u32;
+    let mut low = 0u32;
+    let mut mid = (high + low) >> 1;
+    loop {
+        let remind = s >> mid;
+        if remind > 1 {
+            low = mid;
+            mid = (low + high) >> 1;
+            continue;
+        }
+        if remind == 1 {
+            return mid + 1;
+        }
+        if remind < 1 {
+            high = mid;
+            mid = (low + high) >> 1;
+            continue;
+        }
+    }
+}
+
+/// sqrt num
+pub fn sqrt(s: u128) -> u128 {
+    if s == 0u128 || s == 1 {
+        return s;
+    }
+    let mid_length = bit_length(s) >> 1;
+    let approximate = 1u128 << mid_length;
+    let mut y = s.checked_div(approximate).unwrap();
+    let mut y_0 = 0u128;
+    let throld = 1u128;
+    loop {
+        if y.gt(&y_0) && y.checked_sub(y_0).unwrap().gt(&throld) {
+            let tmp_y = s.checked_div(y).unwrap();
+            y_0 = y;
+            y = y.checked_add(tmp_y).unwrap();
+            y = y >> 1;
+        } else if y.lt(&y_0) && y_0.checked_sub(y).unwrap().gt(&throld) {
+            let tmp_y = s.checked_div(y).unwrap();
+            y_0 = y;
+            y = y.checked_add(tmp_y).unwrap();
+            y = y >> 1;
+        } else {
+            break;
+        }
+    }
+    return y;
+}
+
+/// ln(x)
+pub fn log(s: u128) -> PreciseNumber {
+    let log_arr_1: [u32; 10] = [
+        0, 9531017, 18232155, 26236426, 33647223, 40546510, 47000362, 53062825, 58778666, 64185388,
+    ];
+    let log_arr_2: [u32; 10] = [
+        0, 995033, 1980262, 2955880, 3922071, 4879016, 5826890, 6765864, 7696104, 8617769,
+    ];
+    let log_arr_3: [u32; 10] = [
+        0, 99950, 199800, 299550, 399202, 498754, 598207, 697561, 796816, 895974,
+    ];
+    let log_arr_4: [u32; 10] = [
+        0, 9999, 19998, 29995, 39992, 49987, 59982, 69975, 79968, 89959,
+    ];
+    let log_arr_5: [u32; 10] = [0, 999, 1999, 2999, 3999, 4999, 5999, 6999, 7999, 8999];
+    let log_arr_6: [u32; 10] = [0, 99, 199, 299, 399, 499, 599, 699, 799, 899];
+    let multiply = PreciseNumber::new(100000000u128).unwrap();
+    let length = bit_length(s) - 1;
+    let approximate = 1u128 << length;
+    let s_mul_100000000 = s.checked_mul(100000000u128).expect("log_s_mul_100000000");
+    let s0 = s_mul_100000000 / approximate;
+    let s1 = s0 * 10 / (s0 / 10000000);
+    let index_1 = (s0 / 10000000 - 10) as usize;
+    let s2 = s1 * 100 / (s1 / 1000000);
+    let index_2 = (s1 / 1000000 - 100) as usize;
+    let s3 = s2 * 1000 / (s2 / 100000);
+    let index_3 = (s2 / 100000 - 1000) as usize;
+    let s4 = s3 * 10000 / (s3 / 10000);
+    let index_4 = (s3 / 10000 - 10000) as usize;
+    let s5 = s4 * 100000 / (s4 / 1000);
+    let index_5 = (s4 / 1000 - 100000) as usize;
+    let index_6 = (s5 / 100 - 1000000) as usize;
+    let lnx1 = log_arr_1[index_1] as u128;
+    let lnx2 = log_arr_2[index_2] as u128;
+    let lnx3 = log_arr_3[index_3] as u128;
+    let lnx4 = log_arr_4[index_4] as u128;
+    let lnx5 = log_arr_5[index_5] as u128;
+    let lnx6 = log_arr_6[index_6] as u128;
+    let lnn2 = 69314718 * (length as u128);
+    let tmp_result = lnn2 + lnx1 + lnx2 + lnx3 + lnx4 + lnx5 + lnx6;
+    let result = PreciseNumber::new(tmp_result)
+        .unwrap()
+        .checked_div(&multiply)
+        .unwrap();
+    return result;
+}
+
+/// Compute power of base given exponent in 0.25 increments
+pub fn checked_pow_fraction(base: &PreciseNumber, exp: &PreciseNumber) -> PreciseNumber {
+    let divisor = PreciseNumber::new(1).unwrap()
+        .checked_div(&PreciseNumber::new(4).unwrap()).unwrap();
+
+    let quotient = exp.checked_div(&divisor).expect("quotient")
+        .to_imprecise().unwrap() as i32;
+
+    // index of exponent/0.25
+    match quotient {
+        0 => {
+            // x^0 = 1
+            PreciseNumber::new(1).unwrap()
+        }
+        1 => {
+            // x^1/4 = x^0.25 = ⁴√x = √(√x) = sqrt(sqrt(x))
+            // TODO: decide on using custom square root function, less compute but more inaccurate.
+            // PreciseNumber::new((sqrt(sqrt(base.to_imprecise().unwrap())))).unwrap()
+            (base.sqrt().unwrap()).sqrt().unwrap()
+        }
+        2 => {
+            // x^1/2 = x^0.5 = √x = sqrt(x)
+            // TODO: decide on using custom square root function, less compute but more inaccurate.
+            // PreciseNumber::new(sqrt(base.to_imprecise().unwrap())).unwrap()
+            base.sqrt().unwrap()
+        }
+        4 => {
+            // x^1 = x
+            base.clone()
+        }
+        5 => {
+            // x^5/4 = x^1.25 = x(√(√x)) = x(sqrt(sqrt(x)))
+            base.checked_mul(
+                &(base.sqrt().unwrap()).sqrt().unwrap()
+            ).unwrap()
+        }
+        6 => {
+            // x^3/2 = x^1.50 = x(√x) = x(sqrt(x))
+            base.checked_mul(
+                &base.sqrt().unwrap()
+            ).unwrap()
+        }
+        8 => {
+            // x^2 = 2x
+            base.checked_mul(&base).unwrap()
+        }
+        _ => {
+            assert!(
+                false,
+                "compute_pow not implemented for base: {} exponent: {}",
+                base.value,
+                exp.value
+            );
+            PreciseNumber::new(0u128).unwrap()
+        }
+    }
+}
+
+/// Signed addition
+pub fn signed_addition(lhs: &PreciseNumber, lhs_signed: bool, rhs: &PreciseNumber, rhs_signed: bool) -> (PreciseNumber, bool) {
+    if lhs_signed && rhs_signed {
+        (lhs.checked_add(&rhs).expect("double_negative"), true)
+    } else if lhs_signed && !rhs_signed {
+        rhs.unsigned_sub(&lhs)
+    } else if !lhs_signed && !rhs_signed {
+        (lhs.checked_add(&rhs).expect("double positive"), false)
+    } else {
+        lhs.unsigned_sub(&rhs)
+    }
+}
+
+pub fn signed_mul(lhs: &PreciseNumber, lhs_signed: bool, rhs: &PreciseNumber, rhs_signed: bool) -> (PreciseNumber, bool) {
+    let result = lhs.checked_mul(&rhs).unwrap();
+    let result_signed = !(lhs_signed == rhs_signed);
+    (result, result_signed)
+}
---
+++ libs/hmm/tests/hydra_math_tests.rs
@@ -0,0 +1,224 @@
+#[cfg(test)]
+mod tests {
+    use spl_math::precise_number::{ONE, PreciseNumber};
+    use spl_math::uint::U256;
+    use hydra_math::swap_result::SwapResult;
+    use hydra_math::math::{sqrt, log, checked_pow_fraction, signed_addition, signed_mul};
+
+    type InnerUint = U256;
+
+    #[test]
+    fn test_precise_number_storage() {
+        let pi_numerator = PreciseNumber::new(245850922u128).unwrap();
+        let pi_denominator = PreciseNumber::new(78256779u128).unwrap();
+
+        let swap = SwapResult {
+            k: PreciseNumber::new(1_000_000u128).unwrap(),
+            x_new: PreciseNumber::new(1010u128).unwrap(),
+            y_new: PreciseNumber::new(990u128).unwrap(),
+            delta_x: PreciseNumber::new(10u128).unwrap(),
+            delta_y: pi_numerator.checked_div(&pi_denominator).expect("pi")
+        };
+        // Test imprecise conversion
+        assert_eq!(swap.k.to_imprecise().expect("u128"), 1_000_000u128);
+
+        // Test precision up to 12 decimal places
+        // 3.141592653589
+        let precision = InnerUint::from(ONE);
+        let expected = PreciseNumber { value: InnerUint::from(3_141_592_653_589u128) };
+        assert!(swap.delta_y.almost_eq(&expected, precision));
+    }
+
+    #[test]
+    fn test_u128_square_root() {
+        // largest containing 1 to 9 once
+        let x = 923_187_456u128;
+        assert_eq!(sqrt(x), 30_384u128);
+
+        // largest containing 1 to 9 two times
+        let x = 998_781_235_573_146_624u128;
+        assert_eq!(sqrt(x), 999_390_432u128);
+
+        // largest containing 1 to 9 three times
+        let x = 999_888_767_225_363_175_346_145_124u128;
+        assert_eq!(sqrt(x), 31_621_017_808_182u128);
+
+        let x = u128::MAX;
+        assert_eq!(sqrt(x), 18_446_744_073_709_551_616u128);
+    }
+
+    #[test]
+    fn test_u128_natural_log() {
+        // ln(1) = 0
+        assert_eq!(log(1u128), PreciseNumber::new(0u128).unwrap());
+
+        // ln(1000) = 6.907755278982137
+        let precision = InnerUint::from(ONE);
+        let expected = PreciseNumber { value: InnerUint::from(6_907_755_278_982u128) };
+        assert!(log(1_000u128).almost_eq(&expected, precision));
+
+        // ln(18446744073709551615) = 44.3614195558365
+        let expected = PreciseNumber { value: InnerUint::from(44_361_419_555_836u128) };
+        assert!(log(u64::MAX as u128).almost_eq(&expected, precision));
+
+        // TODO: this overflows
+        // ln(340282366920938463463374607431768211455 = 88.722839111673
+        // assert_eq!(log(u128::MAX), PreciseNumber::new(0u128).unwrap());
+    }
+
+    #[test]
+    fn test_checked_pow_fraction() {
+        let precision = InnerUint::from(ONE);
+        let base = PreciseNumber::new(42u128).unwrap();
+        let exp: PreciseNumber;
+        // 42^0 = 1
+        let exp = PreciseNumber::new(0u128).unwrap();
+        assert_eq!(checked_pow_fraction(&base, &exp), PreciseNumber::new(1u128).unwrap());
+
+        // 42^0.25 = 2.545729895021831
+        let exp = PreciseNumber::new(1u128).unwrap()
+            .checked_div(&PreciseNumber::new(4u128).unwrap()).unwrap();
+        let expected = PreciseNumber { value: InnerUint::from(2_545_729_895_021u128) };
+        assert!(checked_pow_fraction(&base, &exp).almost_eq(&expected, precision));
+
+        // 42^0.5 = 6.48074069840786
+        let exp = PreciseNumber::new(1u128).unwrap()
+            .checked_div(&PreciseNumber::new(2u128).unwrap()).unwrap();
+        let expected = PreciseNumber { value: InnerUint::from(6_480_740_698_407u128) };
+        assert!(checked_pow_fraction(&base, &exp).almost_eq(&expected, precision));
+
+        // 42^1 = 42
+        let exp = PreciseNumber::new(1u128).unwrap();
+        assert_eq!(checked_pow_fraction(&base, &exp), PreciseNumber::new(42u128).unwrap());
+
+        // 42^1.25 = 106.920655590916882
+        let exp = PreciseNumber::new(5u128).unwrap()
+            .checked_div(&PreciseNumber::new(4u128).unwrap()).unwrap();
+        let expected = PreciseNumber { value: InnerUint::from(106_920_655_590_916u128) };
+        assert!(checked_pow_fraction(&base, &exp).almost_eq(&expected, precision));
+
+        // 42^1.5 = 272.19110933313013
+        let exp = PreciseNumber::new(3u128).unwrap()
+            .checked_div(&PreciseNumber::new(2u128).unwrap()).unwrap();
+        let expected = PreciseNumber { value: InnerUint::from(272_191_109_333_130u128) };
+        assert!(checked_pow_fraction(&base, &exp).almost_eq(&expected, precision));
+
+        // 42^2 = 1764
+        let exp = PreciseNumber::new(2u128).unwrap();
+        assert_eq!(checked_pow_fraction(&base, &exp), PreciseNumber::new(1764u128).unwrap());
+    }
+
+    #[test]
+    fn test_signed_addition() {
+        // -4 + -3 = -7
+        let lhs = PreciseNumber::new(4).unwrap();
+        let lhs_signed = true;
+        let rhs = PreciseNumber::new(3).unwrap();
+        let rhs_signed = true;
+        let expected = PreciseNumber::new(7).unwrap();
+        let expected_signed = true;
+        assert_eq!(signed_addition(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // -1 + 1 = 0
+        let lhs = PreciseNumber::new(1).unwrap();
+        let lhs_signed = true;
+        let rhs = PreciseNumber::new(1).unwrap();
+        let rhs_signed = false;
+        let expected = PreciseNumber::new(0).unwrap();
+        let expected_signed = false;
+        assert_eq!(signed_addition(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // 3 + -5 = -2
+        let lhs = PreciseNumber::new(3).unwrap();
+        let lhs_signed = false;
+        let rhs = PreciseNumber::new(5).unwrap();
+        let rhs_signed = true;
+        let expected = PreciseNumber::new(2).unwrap();
+        let expected_signed = true;
+        assert_eq!(signed_addition(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // -5 + 3 = -2
+        let lhs = PreciseNumber::new(5).unwrap();
+        let lhs_signed = true;
+        let rhs = PreciseNumber::new(3).unwrap();
+        let rhs_signed = false;
+        let expected = PreciseNumber::new(2).unwrap();
+        let expected_signed = true;
+        assert_eq!(signed_addition(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // 5 + -2 = 3
+        let lhs = PreciseNumber::new(5).unwrap();
+        let lhs_signed = false;
+        let rhs = PreciseNumber::new(2).unwrap();
+        let rhs_signed = true;
+        let expected = PreciseNumber::new(3).unwrap();
+        let expected_signed = false;
+        assert_eq!(signed_addition(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // -3 + 5 = 2
+        let lhs = PreciseNumber::new(3).unwrap();
+        let lhs_signed = true;
+        let rhs = PreciseNumber::new(5).unwrap();
+        let rhs_signed = false;
+        let expected = PreciseNumber::new(2).unwrap();
+        let expected_signed = false;
+        assert_eq!(signed_addition(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // 1 + -2 = -1
+        let lhs = PreciseNumber::new(1).unwrap();
+        let lhs_signed = false;
+        let rhs = PreciseNumber::new(2).unwrap();
+        let rhs_signed = true;
+        let expected = PreciseNumber::new(1).unwrap();
+        let expected_signed = true;
+        assert_eq!(signed_addition(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // 4 + 3 = 7
+        let lhs = PreciseNumber::new(4).unwrap();
+        let lhs_signed = false;
+        let rhs = PreciseNumber::new(3).unwrap();
+        let rhs_signed = false;
+        let expected = PreciseNumber::new(7).unwrap();
+        let expected_signed = false;
+        assert_eq!(signed_addition(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+    }
+
+    #[test]
+    fn test_signed_mul() {
+        // -4 * -3 = 12
+        let lhs = PreciseNumber::new(4).unwrap();
+        let lhs_signed = true;
+        let rhs = PreciseNumber::new(3).unwrap();
+        let rhs_signed = true;
+        let expected = PreciseNumber::new(12).unwrap();
+        let expected_signed = false;
+        assert_eq!(signed_mul(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // -4 * 3 = -12
+        let lhs = PreciseNumber::new(4).unwrap();
+        let lhs_signed = true;
+        let rhs = PreciseNumber::new(3).unwrap();
+        let rhs_signed = false;
+        let expected = PreciseNumber::new(12).unwrap();
+        let expected_signed = true;
+        assert_eq!(signed_mul(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // 4 * -3 = -12
+        let lhs = PreciseNumber::new(4).unwrap();
+        let lhs_signed = false;
+        let rhs = PreciseNumber::new(3).unwrap();
+        let rhs_signed = true;
+        let expected = PreciseNumber::new(12).unwrap();
+        let expected_signed = true;
+        assert_eq!(signed_mul(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+
+        // 4 * 3 = 12
+        let lhs = PreciseNumber::new(4).unwrap();
+        let lhs_signed = false;
+        let rhs = PreciseNumber::new(3).unwrap();
+        let rhs_signed = false;
+        let expected = PreciseNumber::new(12).unwrap();
+        let expected_signed = false;
+        assert_eq!(signed_mul(&lhs, lhs_signed, &rhs, rhs_signed), (expected, expected_signed));
+    }
+}
