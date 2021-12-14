db001a09dd22d15530851ad2370183c77205425e
WebDev717 <lasse.lin717@gmail.com>
2021-12-14 23:02:11 +1100

    setup cli opts

--- tools/cli/src/main.rs
+++ tools/cli/src/main.rs
@@ -3,27 +3,50 @@ mod pools;
 mod staking;
 mod utils;
 
-use std::env;
-use anchor_client::Cluster;
+use crate::farming::{execute_stake_lp_tokens_tx, execute_unstake_lp_tokens_tx, FarmingSubCommand};
+use crate::pools::{
+    execute_deposit_tx, execute_init_tx, execute_swap_tx, execute_withdraw_tx, PoolsSubCommand,
+};
+use crate::staking::{execute_stake_tokens_tx, StakingSubCommand};
+use crate::utils::{load_connection, load_keypair};
 use anchor_client::solana_sdk::signature::Keypair;
+use anchor_client::Cluster;
 use dotenv::dotenv;
 use solana_program::pubkey::Pubkey;
 use static_pubkey::static_pubkey;
+use std::env;
+use std::path::PathBuf;
 use structopt::StructOpt;
-use crate::farming::{FarmingSubCommand, execute_stake_lp_tokens_tx, execute_unstake_lp_tokens_tx};
-use crate::pools::{PoolsSubCommand, execute_deposit_tx, execute_init_tx, execute_withdraw_tx, execute_swap_tx};
-use crate::staking::{execute_stake_tokens_tx, StakingSubCommand};
-use crate::utils::{load_connection, load_keypair};
+use strum::VariantNames;
+use strum_macros::EnumString;
+use strum_macros::EnumVariantNames;
 
 const DEFAULT_KEYPAIR: &str = "~/.config/solana/id.json";
 
 #[derive(Debug, StructOpt)]
 pub struct Opt {
+    #[structopt(short = "m", long, case_insensitive = true,possible_values = Moniker::VARIANTS, default_value = DEFAULT_MONIKER)]
+    moniker: Moniker,
+
+    #[structopt(short = "k", long, parse(from_os_str))]
+    keypair: PathBuf,
+
     #[structopt(subcommand)]
     pub cmd: SubCommand,
 }
 
-#[derive(Debug,StructOpt)]
+const DEFAULT_MONIKER: &str = "localhost";
+
+#[derive(EnumString, EnumVariantNames, Debug)]
+#[strum(serialize_all = "kebab_case")]
+pub enum Moniker {
+    Localhost,
+    Devnet,
+    Testnet,
+    Mainnet,
+}
+
+#[derive(Debug, StructOpt)]
 pub enum SubCommand {
     Pools {
         #[structopt(subcommand)]
@@ -36,7 +59,7 @@ pub enum SubCommand {
     Staking {
         #[structopt(subcommand)]
         cmd: StakingSubCommand,
-    }
+    },
 }
 
 fn main() -> anyhow::Result<()> {
@@ -61,9 +84,9 @@ fn main() -> anyhow::Result<()> {
                     execute_swap_tx(&connection)?;
                 }
             }
-        },
+        }
         SubCommand::Farming { cmd } => {
-            let connection = load_connection(hydra_farming::ID, keypair,cluster)?;
+            let connection = load_connection(hydra_farming::ID, keypair, cluster)?;
             match cmd {
                 FarmingSubCommand::StakeLpTokens => {
                     execute_stake_lp_tokens_tx(&connection)?;
@@ -72,15 +95,15 @@ fn main() -> anyhow::Result<()> {
                     execute_unstake_lp_tokens_tx(&connection)?;
                 }
             }
-        },
+        }
         SubCommand::Staking { cmd } => {
-            let connection = load_connection(hydra_staking::ID, keypair,cluster)?;
+            let connection = load_connection(hydra_staking::ID, keypair, cluster)?;
             match cmd {
                 StakingSubCommand::StakeTokens => {
                     execute_stake_tokens_tx(&connection)?;
                 }
             }
-        },
+        }
     }
     Ok(())
 }
