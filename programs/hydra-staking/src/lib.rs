0ebcc5f027f623e48c63cd52bf5330ee3bc354f7
WebDev717 <lasse.lin717@gmail.com>
2022-01-10 16:54:49 +1100

    Setup initialize instruction

--- programs/hydra-staking/src/lib.rs
+++ programs/hydra-staking/src/lib.rs
@@ -1,6 +1,22 @@
+mod instructions;
+mod state;
+
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("F1y1FTP91nwxbNUW3nXY6mKQzWawihwVYGwMsi7oKGyg");
+
+#[cfg(feature = "localnet")]
+pub mod constants {
+    pub const HYDRA_TOKEN_MINT_PUBKEY: &str = "";
+    pub const X_HYDRA_TOKEN_MINT_PUBKEY: &str = "";
+}
+
+#[cfg(feature = "devnet")]
+pub mod constants {
+    pub const HYDRA_TOKEN_MINT_PUBKEY: &str = "";
+    pub const X_HYDRA_TOKEN_MINT_PUBKEY: &str = "";
+}
 
 #[program]
 pub mod hydra_staking {
@@ -11,4 +27,26 @@ pub mod hydra_staking {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(nonce: u8)]
+pub struct Initialize<'info> {
+    pub token_mint: Box<Account<'info, Mint>>,
+
+    #[account(
+        init,
+        payer = initializer,
+        token::mint = token_mint,
+        token::authority = token_vault,
+        seeds = [ constants::HYDRA_TOKEN_MINT_PUBKEY.parse::<Pubkey>().unwrap().as_ref() ],
+        bump = nonce,
+    )]
+    /// the not-yet-created, derived token vault pubkey. PDA
+    pub token_vault: Box<Account<'info, TokenAccount>>,
+
+    // #[account(mut)] TODO Review if needed.
+    pub initializer: Signer<'info>,
+
+    /// required by anchor for init of the token_vault
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
