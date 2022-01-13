4161459b01284cff0af50f636dc1cb8a8c5bb2ea
WebDev717 <lasse.lin717@gmail.com>
2022-01-13 14:00:39 +1100

    refactors

--- tests/hydra-staking/hydra-staking.ts
+++ tests/hydra-staking/hydra-staking.ts
@@ -1,44 +1,102 @@
 import * as anchor from '@project-serum/anchor';
-import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import {TOKEN_PROGRAM_ID, createMint, getOrCreateAssociatedTokenAccount, mintTo} from "@solana/spl-token";
 import {BN, Program, web3} from '@project-serum/anchor';
 import { HydraStaking } from '../../target/types/hydra_staking';
-// import {createMint,  getMintInfo} from "@project-serum/common";
-import { loadKey, createMintAndVault } from "../utils/utils"
-import {Account} from "@solana/web3.js";
-import {getMintInfo} from "@project-serum/common";
+import { loadKey } from "../utils/utils"
 
 describe('hydra-staking',  () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
-  const program = anchor.workspace.HydraStaking as Program<HydraStaking>;
+    anchor.setProvider(anchor.Provider.env());
+    const program = anchor.workspace.HydraStaking as Program<HydraStaking>;
 
-  let hydMint
-  let hydVault
+    let hydMintKeyPair
 
-  it('should mint Hyd', async () => {
-    hydMint = await loadKey("tests/keys/hyd3VthE9YPGBeg9HEgZsrM5qPniC6VoaEFeTGkVsJR.json")
-    hydVault = web3.Keypair.generate();
+    let vaultPubkey
+    let vaultBump
 
-    await createMintAndVault(program.provider, hydMint, hydVault, new BN(100_000_0000))
-  });
+    it('should mint Hyd', async () => {
+        // Create signer account
+        let signer = anchor.web3.Keypair.generate()
 
-  it('should initialized stake PDA vault', async () => {
-    let [vaultPubkey, vaultBump] = await anchor.web3.PublicKey.findProgramAddress(
-        [hydMint.publicKey.toBuffer()],
-        program.programId
-    )
+        // airdrop sol to signer account
+        await program.provider.connection.confirmTransaction(
+            await program.provider.connection.requestAirdrop(signer.publicKey, 10000000000),
+            "confirmed"
+        )
 
-    await program.rpc.initialize(
-        vaultBump,
-        {
-            accounts: {
-              tokenMint: hydMint.publicKey,
-              tokenVault: vaultPubkey,
-              initializer: program.provider.wallet.publicKey,
-              systemProgram: anchor.web3.SystemProgram.programId,
-              tokenProgram: TOKEN_PROGRAM_ID,
-              rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        // load keyPair
+        hydMintKeyPair = await loadKey("tests/keys/hyd3VthE9YPGBeg9HEgZsrM5qPniC6VoaEFeTGkVsJR.json")
+
+        // create mint with keyPair
+        await createMint(
+            program.provider.connection,
+            signer,
+            signer.publicKey,
+            signer.publicKey,
+            9,
+            hydMintKeyPair,
+        )
+
+        // create AssociatedTokenAccount for signer
+        let assTokenAccount = await getOrCreateAssociatedTokenAccount(
+            program.provider.connection,
+            signer,
+            hydMintKeyPair.publicKey,
+            signer.publicKey
+        )
+
+        // mint to signer AssociatedTokenAccount
+        await mintTo(
+            program.provider.connection,
+            signer,
+            hydMintKeyPair.publicKey,
+            assTokenAccount.address,
+            signer,
+            100000000,
+        )
+    })
+
+    it('should initialized stake PDA vault', async () => {
+       [vaultPubkey, vaultBump] = await anchor.web3.PublicKey.findProgramAddress(
+            [hydMintKeyPair.publicKey.toBuffer()],
+            program.programId
+        )
+
+        await program.rpc.initialize(
+            vaultBump,
+            {
+                accounts: {
+                tokenMint: hydMintKeyPair.publicKey,
+                tokenVault: vaultPubkey,
+                initializer: program.provider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+                tokenProgram: TOKEN_PROGRAM_ID,
+                rent: anchor.web3.SYSVAR_RENT_PUBKEY,
             }
-          });
-  });
+            });
+    });
+
+    // it('should create xhyd mint', async () => {
+    //     xhydMint = await loadKey("tests/keys/xhy1rv75cEJahTbsKnv2TpNhdR7KNUoDPavKuQDwhDU.json")
+    //     // let mintPubkey = await createMint(program.provider, xhydMint)
+    //     // console.log("xdyMint: ", xhydMint)
+    //     // console.log(await getMintInfo(program.provider,xhydMint))
+    // });
+    //
+    // it('should stake tokens into vault', async () => {
+    //
+    //     await program.rpc.stake(
+    //         vaultBump,
+    //         new anchor.BN(100),
+    //         {
+    //           accounts: {
+    //               tokenMint: hydMint.publicKey,
+    //               xTokenMint: xhydMint.publicKey,
+    //               tokenFrom: hydWalletAccount,
+    //               tokenFromAuthority: program.provider.wallet.publicKey,
+    //               tokenVault: vaultPubkey,
+    //               xTokenTo: anchor.web3.Keypair.generate(),
+    //               tokenProgram: TOKEN_PROGRAM_ID,
+    //           }
+    //         })
+    //   });
 });
--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -2,9 +2,6 @@ import * as Buffer from "buffer";
 import { URL } from "url";
 import * as fs from "fs";
 import * as anchor from '@project-serum/anchor';
-import {BN, Provider} from "@project-serum/anchor";
-import { Keypair,PublicKey, Account, Transaction, SystemProgram } from "@solana/web3.js";
-import { TokenInstructions } from '@project-serum/serum';
 
 type PathLike = string | Buffer | URL;
 
@@ -14,52 +11,3 @@ export async function loadKey(path: PathLike) :Promise<anchor.web3.Keypair> {
     return anchor.web3.Keypair.fromSecretKey(new Uint8Array(keydata))
 }
 
-export async function createMintAndVault(
-    provider: Provider,
-    mint: Keypair,
-    vault: Keypair,
-    amount: BN,
-    owner?: PublicKey,
-    decimals?: number,
-): Promise<[PublicKey, PublicKey]> {
-    if (owner === undefined) {
-        owner = provider.wallet.publicKey;
-    }
-    const tx = new Transaction();
-    tx.add(
-        SystemProgram.createAccount({
-            fromPubkey: provider.wallet.publicKey,
-            newAccountPubkey: mint.publicKey,
-            space: 82,
-            lamports: await provider.connection.getMinimumBalanceForRentExemption(82),
-            programId: TokenInstructions.TOKEN_PROGRAM_ID,
-        }),
-        TokenInstructions.initializeMint({
-            mint: mint.publicKey,
-            decimals: decimals ?? 0,
-            mintAuthority: provider.wallet.publicKey,
-        }),
-        SystemProgram.createAccount({
-            fromPubkey: provider.wallet.publicKey,
-            newAccountPubkey: vault.publicKey,
-            space: 165,
-            lamports: await provider.connection.getMinimumBalanceForRentExemption(
-                165,
-            ),
-            programId: TokenInstructions.TOKEN_PROGRAM_ID,
-        }),
-        TokenInstructions.initializeAccount({
-            account: vault.publicKey,
-            mint: mint.publicKey,
-            owner,
-        }),
-        TokenInstructions.mintTo({
-            mint: mint.publicKey,
-            destination: vault.publicKey,
-            amount,
-            mintAuthority: provider.wallet.publicKey,
-        }),
-    );
-    await provider.send(tx, [mint, vault]);
-    return [mint.publicKey, vault.publicKey];
-}
