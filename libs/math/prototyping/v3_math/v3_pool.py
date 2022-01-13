80aa7b648dca1e0e6c1b845470bd95680a587fa3
WebDev717 <lasse.lin717@gmail.com>
2022-01-13 15:45:56 +0000

    hmm-cl: apply/offset calculated fees on deposit & withdrawal. separately track hmm_adj_fee and swap_fee

--- libs/math/prototyping/v3_math/v3_pool.py
+++ libs/math/prototyping/v3_math/v3_pool.py
@@ -23,8 +23,10 @@ class GlobalState:
     tick: int = 0  # current tick
     fg_x: float = 0.0  # fee growth global
     fg_y: float = 0.0  # fee growth global
-    proto_x: float = 0.0  # protocol fees
-    proto_y: float = 0.0  # protocol fees
+    hg_x: float = 0.0  # fee growth global
+    hg_y: float = 0.0  # fee growth global
+    # proto_x: float = 0.0  # protocol fees
+    # proto_y: float = 0.0  # protocol fees
 
 
 @dataclass
@@ -35,13 +37,15 @@ class TickState:
     liq_gross: float = 0.0  # LiquidityGross
     f0_x: float = 0.0  # feegrowth outside
     f0_y: float = 0.0  # feegrowth outside
+    h0_x: float = 0.0  # hmm adj-fee growth outside
+    h0_y: float = 0.0  # hmm adj-fee growth outside
     # TODO : implem s0, i0, sl0 in Tick-state
-    s0_x: float = 0.0  # seconds (time) outside
-    s0_y: float = 0.0  # seconds (time) outside
-    i0_x: float = 0.0  # tickCumulative outside
-    i0_y: float = 0.0  # tickCumulative outside
-    sl0_x: float = 0.0  # secondsPerLiqudity outside
-    sl0_y: float = 0.0  # secondsPerLiqudity outside
+    # s0_x: float = 0.0  # seconds (time) outside
+    # s0_y: float = 0.0  # seconds (time) outside
+    # i0_x: float = 0.0  # tickCumulative outside
+    # i0_y: float = 0.0  # tickCumulative outside
+    # sl0_x: float = 0.0  # secondsPerLiqudity outside
+    # sl0_y: float = 0.0  # secondsPerLiqudity outside
 
 
 @dataclass
@@ -51,6 +55,8 @@ class PositionState:
     liq: float = 0.0  # liquidity
     fr_x: float = 0.0  # feegrowth inside last
     fr_y: float = 0.0  # feegrowth inside last
+    hr_x: float = 0.0  # hmm adj-fee growth inside last
+    hr_y: float = 0.0  # hmm adj-fee growth inside last
 
 
 class Pool:
@@ -120,15 +126,20 @@ class Pool:
 
         return "\n".join(lines)
 
-    def show(self):
+    def show(self, ticks=True, positions=True):
+        print()
         print(f"{self.global_state}\nreal reserves X={self.X} Y={self.Y}")
         print(f"cumulative HMM X_adj={self.X_adj} Y_adj={self.Y_adj}")
-        print("---active ticks---")
-        for k, v in self.active_ticks.items():
-            print(f"tick '{k}': {v}")
-        print("---positions---")
-        for k, v in self.positions.items():
-            print(f"poz '{k}': {v}")
+        print(f"cumulative swap fees_X={self.X_fee} fees_Y={self.Y_fee}")
+
+        if ticks:
+            print("---active ticks---")
+            for k, v in self.active_ticks.items():
+                print(f"tick '{k}': {v}")
+        if positions:
+            print("---positions---")
+            for k, v in self.positions.items():
+                print(f"poz '{k}': {v}")
 
     @staticmethod
     def liq_x_only(x, rPa, rPb):
@@ -281,9 +292,12 @@ class Pool:
         # set f0 of tick based on convention [6.21]
         f0_x = self.global_state.fg_x if self.global_state.tick >= tick else 0
         f0_y = self.global_state.fg_y if self.global_state.tick >= tick else 0
+
+        h0_x = self.global_state.hg_x if self.global_state.tick >= tick else 0
+        h0_y = self.global_state.hg_y if self.global_state.tick >= tick else 0
         # TODO : to the same to s0, i0 and sl0 of tick state
 
-        ts = TickState(f0_x=f0_x, f0_y=f0_y)
+        ts = TickState(f0_x=f0_x, f0_y=f0_y, h0_x=h0_x, h0_y=h0_y)
         self.active_ticks[tick] = ts
 
         return ts
@@ -426,6 +440,9 @@ class Pool:
         ts.f0_x = self.global_state.fg_x - ts.f0_x
         ts.f0_y = self.global_state.fg_y - ts.f0_y
 
+        ts.h0_x = self.global_state.hg_x - ts.h0_x
+        ts.h0_y = self.global_state.hg_y - ts.h0_y
+
         # TODO: do the same for s0, i0, sl0 in Tick-state
 
         # update current tick in global state to reflect crossing; rP unchanged
@@ -436,11 +453,55 @@ class Pool:
                 provided_tick - 1, left_to_right
             )
 
+    def fee_below_above(self, tick, for_x=True, swp=True):
+        """Fees earned in a token below and above tick, as tuple.
+        can compute for either token, and for either swap fees or hmm fees"""
+
+        token = "x" if for_x else "y"
+        which = "f" if swp else "h"
+
+        i_c = self.global_state.tick
+        fg = getattr(self.global_state, f"{which}g_{token}")
+        ts: TickState = self.active_ticks.get(tick, None)
+        if ts is None:
+            # raise Exception("cannot find tick for fee calc")
+            return (fg, 0.0)  # from [6.17 - 6.21], convention
+
+        f0 = getattr(ts, f"{which}0_{token}")  # f0_x or f0_y or h0_x or h0_y
+
+        f_below = f0 if i_c >= tick else fg - f0
+        f_above = fg - f0 if i_c >= tick else f0
+
+        return (f_below, f_above)
+
+    def fee_rng(self, lower_tick, upper_tick, for_x=True, swp=True):
+        f_blw_lwr, f_abv_lwr = self.fee_below_above(lower_tick, for_x, swp)
+        f_blw_upr, f_abv_upr = self.fee_below_above(upper_tick, for_x, swp)
+
+        # retrieve fg by summing up either tuple, they should match
+        assert f_blw_lwr + f_abv_lwr == f_blw_upr + f_abv_upr
+        fg = f_blw_lwr + f_abv_lwr
+
+        return fg - f_blw_lwr - f_abv_upr
+
     def _set_position(self, user_id, lower_tick, upper_tick, liq_delta):
         """handles all facets for updates a position for in the pool,
         used for deposits (l>0), withdrawals (l<0)"""
-        # TODO :compute the uncollected fees f_u the poz is entitled to
-        new_fr_x, new_fr_y = 0.0, 0.0
+        # compute the uncollected fees f_u the poz is entitled to
+        # first compute new FeeGrowthInside, to be written to position
+        # will be set to 0 by function if tick not initialized
+        new_fr_x = self.fee_rng(lower_tick, upper_tick, for_x=True)
+        new_fr_y = self.fee_rng(lower_tick, upper_tick, for_x=False)
+
+        new_hr_x = self.fee_rng(lower_tick, upper_tick, for_x=True, swp=False)
+        new_hr_y = self.fee_rng(lower_tick, upper_tick, for_x=False, swp=False)
+
+        # then get old value from when position was last touched.(set below)
+        # set to 0 as default for when new position
+        old_fr_x, old_fr_y = 0.0, 0.0
+        old_hr_x, old_hr_y = 0.0, 0.0
+        # liquidity to use for computing fee amounts (set below)
+        base = 0.0
 
         # find position if exists
         # positions are uniquely identitfied by the (sender, lower, upper)
@@ -453,9 +514,14 @@ class Pool:
                 raise Exception("cannot newly provide negative liquidity")
             # creates a new position
             self.positions[key] = PositionState(
-                liq=liq_delta, fr_x=0.0, fr_y=0.0
+                liq=liq_delta, fr_x=0.0, fr_y=0.0, hr_x=0.0, hr_y=0.0
             )
         else:
+            # get old value for feed from when position was last touched
+            old_fr_x, old_fr_y = poz.fr_x, poz.fr_y
+            old_hr_x, old_hr_y = poz.hr_x, poz.hr_y
+            base = poz.liq
+
             # update existing position
             if liq_delta < 0.0 and poz.liq + liq_delta < 0.0:
                 # abort if withdrawal liq exceeds position liquidity
@@ -466,8 +532,19 @@ class Pool:
                 del self.positions[key]
             else:
                 self.positions[key] = PositionState(
-                    liq=poz.liq + liq_delta, fr_x=new_fr_x, fr_y=new_fr_y
+                    liq=poz.liq + liq_delta,
+                    fr_x=new_fr_x,
+                    fr_y=new_fr_y,
+                    hr_x=new_hr_x,
+                    hr_y=new_hr_y,
                 )
+        # now calulate uncollected fees to be applied
+        f_u_x = new_fr_x - old_fr_x
+        f_u_y = new_fr_y - old_fr_y
+        h_u_x = new_hr_x - old_hr_x
+        h_u_y = new_hr_y - old_hr_y
+        assert f_u_x >= 0.0 and f_u_y >= 0.0
+        assert h_u_x >= 0.0 and h_u_y >= 0.0
 
         # update tick states for lower and upper
         self.update_tick(lower_tick, liq_delta, upper=False)
@@ -479,6 +556,9 @@ class Pool:
             and self.global_state.tick < upper_tick
         ):
             self.global_state.L += liq_delta
+
+        # return uncollected fee amounts to offset/add in deposit/withdrawal
+        return base * f_u_x, base * f_u_y, base * h_u_x, base * h_u_y
 
     def deposit(self, user_id, x, y, rPa, rPb):
         """interface to deposit liquidity in pool & give change if necessary"""
@@ -503,16 +583,30 @@ class Pool:
         # TODO choose what precision to round down to.
         liq = math.floor(liq)
 
-        x_dep = Pool.x_from_L_rP_rng(liq, rP, rPa_used, rPb_used)
-        y_dep = Pool.y_from_L_rP_rng(liq, rP, rPa_used, rPb_used)
+        x_in = Pool.x_from_L_rP_rng(liq, rP, rPa_used, rPb_used)
+        y_in = Pool.y_from_L_rP_rng(liq, rP, rPa_used, rPb_used)
         # TODO round up amount deposited if necessary
-        assert x_dep <= x, "used x amt cannot excess provided amount"
-        assert y_dep <= y, "used y amt cannot excess provided amount"
+        assert x_in <= x, "used x amt cannot excess provided amount"
+        assert y_in <= y, "used y amt cannot excess provided amount"
 
-        self._set_position(user_id, lower_tick, upper_tick, liq_delta=liq)
-        self.X += x_dep
-        self.Y += y_dep
-        print(f"{x_dep=} {y_dep=} X returned {x-x_dep} Y returned {y-y_dep}")
+        fees_x, fees_y, adj_x, adj_y = self._set_position(
+            user_id, lower_tick, upper_tick, liq_delta=liq
+        )
+        # offset fee amounts from deposit amounts
+        # this will be the amount debited from user
+        x_debited = x_in - fees_x - adj_x
+        y_debited = y_in - fees_y - adj_y
+        # update state: reserves, fee pot , hmm-adj-fee pot
+        self.X += x_in
+        self.Y += y_in
+        self.X_fee -= fees_x
+        self.X_adj -= adj_x
+        self.Y_fee -= fees_y
+        self.Y_adj -= adj_y
+        print()
+        print(f"{x_debited=} {y_debited=}")
+        print(f"including {fees_x+adj_x=} and {fees_y+adj_y=}")
+        print(f"X returned {x-x_debited} Y returned {y-y_debited}")
 
     def withdraw(self, user_id, liq, rPa, rPb):
         """interface to withdraw liquidity from pool"""
@@ -527,7 +621,9 @@ class Pool:
         rPa_used = Pool.tick_to_rP(lower_tick)
         rPb_used = Pool.tick_to_rP(upper_tick)
 
-        self._set_position(user_id, lower_tick, upper_tick, liq_delta=-liq)
+        fees_x, fees_y, adj_x, adj_y = self._set_position(
+            user_id, lower_tick, upper_tick, liq_delta=-liq
+        )
 
         rP = self.global_state.rP
         # TODO should we use Oracle price here instead? or real price as param
@@ -539,9 +635,19 @@ class Pool:
         x_out *= 1 - Pool.ADJ_WITHDRAWAL
         y_out *= 1 - Pool.ADJ_WITHDRAWAL
 
+        # add fees on to what user will receive
+        x_sent = x_out + fees_x + adj_x
+        y_sent = y_out + fees_y + adj_y
+        # update state: reserves, fee pot , hmm-adj-fee pot
         self.X -= x_out
         self.Y -= y_out
-        print(f"{x_out=} {y_out=}")
+        self.X_fee -= fees_x
+        self.X_adj -= adj_x
+        self.Y_fee -= fees_y
+        self.Y_adj -= adj_y
+        print()
+        print(f"{x_sent=} {y_sent=}")
+        print(f"including {fees_x+adj_x=} & {fees_y+adj_y=}")
 
     def swap_within_tick_from_X(
         self, start_rP, goal_tick, L, dX, rP_oracle=None
@@ -684,7 +790,7 @@ class Pool:
             if goal_tick is None:
                 # there are no more active ticks on the left, terminate swap
                 print("no more active ticks (liquidity) in this direction")
-                avg_P = -swpd_dY / swpd_dX if swpd_dX != 0.0 else None
+                avg_P = -swpd_dY / swpd_dX if swpd_dX != 0.0 else 0.0
                 end_P = self.global_state.rP ** 2
                 print(
                     f"{swpd_dX=} {swpd_dY=} pool_X={self.X} pool_Y={self.Y} "
@@ -731,6 +837,11 @@ class Pool:
             self.Y += done_dY - hmm_adj_Y  # adj out of reserves into fees
             self.X_fee += fee_X
             self.Y_adj += hmm_adj_Y
+            if self.global_state.L > 0.0:
+                # make sure not 0 liquidity (empty trade)
+                # * update fee growth to reflect latest swap_within
+                self.global_state.fg_x += fee_X / self.global_state.L
+                self.global_state.hg_y += hmm_adj_Y / self.global_state.L
 
             if cross is True:
                 assert tmp_tick == goal_tick
@@ -889,7 +1000,7 @@ class Pool:
             if goal_tick is None:
                 # there are no more active ticks on the left, terminate swap
                 print("no more active ticks (liquidity) in this direction")
-                avg_P = -swpd_dY / swpd_dX if swpd_dX != 0.0 else None
+                avg_P = -swpd_dY / swpd_dX if swpd_dX != 0.0 else 0.0
                 end_P = self.global_state.rP ** 2
                 print(
                     f"{swpd_dX=} {swpd_dY=} pool_X={self.X} pool_Y={self.Y} "
@@ -940,6 +1051,11 @@ class Pool:
             self.Y += done_dY
             self.X_adj += hmm_adj_X
             self.Y_fee += fee_Y
+            if self.global_state.L > 0.0:
+                # make sure not 0 liquidity (empty trade)
+                # * update fee growth to reflect latest swap_within
+                self.global_state.hg_x += hmm_adj_X / self.global_state.L
+                self.global_state.fg_y += fee_Y / self.global_state.L
 
             if cross is True:
                 assert tmp_tick == goal_tick
