bd2c308c658562e4a35b08d1882e0c22f01b1635
WebDev717 <lasse.lin717@gmail.com>
2022-01-26 15:18:14 +1100

    fixed naming and used the pool_state.token_mint_decimals for the calulation.

--- programs/hydra-staking/src/utils/price.rs
+++ programs/hydra-staking/src/utils/price.rs
@@ -1,24 +1,28 @@
-use anchor_lang::prelude::Account;
+use crate::state::pool_state::PoolState;
+use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, TokenAccount};
-const ONE_E9: u64 = 1000000000;
 
 /// calculate price
 pub fn calculate_price<'info>(
     vault: &Account<'info, TokenAccount>,
     mint: &Account<'info, Mint>,
+    pool_state: &Account<'info, PoolState>,
 ) -> u64 {
     let total_vault_token = vault.amount;
-    let total_x_token = mint.supply;
+    let total_redeemable_token = mint.supply;
 
-    if total_x_token == 0 {
+    if total_redeemable_token == 0 {
+        msg!("total_redeemable_token: 0");
         return 0;
     }
 
-    // (total_vault_token * 1e9 ) / total_x_token.supply
+    let decimals = 10u64.pow(pool_state.token_mint_decimals as u32);
+
+    // (total_vault_token * 10^9 ) / total_x_token.supply
     let price_uint = total_vault_token
-        .checked_mul(ONE_E9)
+        .checked_mul(decimals)
         .unwrap()
-        .checked_div(total_x_token)
+        .checked_div(total_redeemable_token)
         .unwrap();
 
     price_uint
