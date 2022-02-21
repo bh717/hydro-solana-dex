d5ef450f4e8b0e7a1bdb60fd98f83f4bdf6de33a
WebDev717 <lasse.lin717@gmail.com>
2022-02-21 18:19:39 +1000

    Consolidate tsconfigs

--- sdks/hydra-ts/jest.config.js
+++ sdks/hydra-ts/jest.config.js
@@ -1,6 +1,1 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/src/**/?(*.)+(spec|test).ts?(x)"],
-};
+module.exports = require("config-ts/jest.config");
