9cf05a092df1771f8be7e905d1541d6045b7956d
WebDev717 <lasse.lin717@gmail.com>
2022-02-23 15:01:07 +1100

    removed

--- scripts/remove_element.js
+++
@@ -1,9 +0,0 @@
-
-const ts = require('typescript')
-const node = ts.createSourceFile(
-    'x.ts',
-    fs.readFileSync('./sdks/hydra-ts/codegen/types/hydra_liquidity_pools.ts', 'utf8'),
-    ts.ScriptTarget.Latest
-);
-
-console.log(node)
