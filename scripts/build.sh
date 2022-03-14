7757e9ebd26a07e2a078134f292bf037616c128e
WebDev717 <lasse.lin717@gmail.com>
2022-03-14 21:06:35 +1000

    Tidy up build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,20 +13,5 @@ rm -rf ./sdks/types-ts/codegen/types/*.ts
 # build new
 anchor build -- --features "localnet"
 
-# copy new
-# cp ./target/types/*.ts ./sdks/types-ts/codegen/types
-
+# process idl
 yarn ts-node ./scripts/process-idl.ts
-
-# yarn ts-node ./scripts/process-idl.ts \
-#   --input ./target/idl/hydra_liquidity_pools.json \
-#   --output ./sdks/types-ts/codegen/types/hydra_liquidity_pools.ts
-
-# yarn ts-node ./scripts/process-idl.ts \
-#   --input ./target/idl/hydra_liquidity_pools.json \
-#   --output ./sdks/types-ts/codegen/types/hydra_liquidity_pools.ts
-
-
-# process new codegen and types files.
-
-# TODO Remove reserve from ts types file
