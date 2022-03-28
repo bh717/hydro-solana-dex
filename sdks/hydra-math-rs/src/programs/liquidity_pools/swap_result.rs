bb895687fa092935d97fe6c26c877b149b1c12ed
WebDev717 <lasse.lin717@gmail.com>
2022-03-28 14:12:11 +0800

    swap result should return scaled u64 to client

--- sdks/hydra-math-rs/src/programs/liquidity_pools/swap_result.rs
+++ sdks/hydra-math-rs/src/programs/liquidity_pools/swap_result.rs
@@ -1,56 +1,23 @@
 //! Result storage
 
-use crate::decimal::Decimal;
+use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
 #[derive(Default, Debug)]
 /// Encodes all results of swapping from a source token to a destination token
 pub struct SwapResult {
     /// Invariant expressed as k
-    pub k: Decimal,
+    pub k: u64,
     /// squared_k is the k version used to calculate the lp tokens
-    pub squared_k: Decimal,
+    pub squared_k: u64,
     /// New base token amount expressed as x_new
-    pub x_new: Decimal,
+    pub x_new: u64,
     /// New quote token amount expressed as y_new
-    pub y_new: Decimal,
+    pub y_new: u64,
     /// Amount of base token swapped expressed as delta_x
-    pub delta_x: Decimal,
+    pub delta_x: u64,
     /// Amount of quote token swapped expressed as delta_x
-    pub delta_y: Decimal,
+    pub delta_y: u64,
     /// Amount of fees deducted from source token before operation
-    pub fees: Decimal,
-}
-
-impl SwapResult {
-    pub fn x_new_down(&self) -> u64 {
-        self.x_new.to_scale(0).try_into().unwrap()
-    }
-
-    pub fn x_new_up(&self) -> u64 {
-        self.x_new.to_scale_up(0).try_into().unwrap()
-    }
-
-    pub fn y_new_down(&self) -> u64 {
-        self.y_new.to_scale(0).try_into().unwrap()
-    }
-
-    pub fn y_new_up(&self) -> u64 {
-        self.y_new.to_scale_up(0).try_into().unwrap()
-    }
-
-    pub fn delta_x_down(&self) -> u64 {
-        self.delta_x.to_scale(0).try_into().unwrap()
-    }
-
-    pub fn delta_y_down(&self) -> u64 {
-        self.delta_y.to_scale(0).try_into().unwrap()
-    }
-
-    pub fn squared_k_up(&self) -> u64 {
-        self.squared_k.to_scale_up(0).try_into().unwrap()
-    }
-
-    pub fn squared_k_down(&self) -> u64 {
-        self.squared_k.to_scale(0).try_into().unwrap()
-    }
+    pub fees: u64,
 }
