ca606fe19fad73e6d4f1de944a78342795c80b8f
WebDev717 <lasse.lin717@gmail.com>
2022-04-09 20:15:02 +0800

    refactor for exponential approximation

--- sdks/hydra-math-rs/src/programs/fees/volatility_adjusted_fee.rs
+++ sdks/hydra-math-rs/src/programs/fees/volatility_adjusted_fee.rs
@@ -1,36 +1,66 @@
 use crate::decimal::{Add, Compare, Decimal, Div, Mul, Pow, Sub};
 use crate::programs::fees::error::FeeCalculatorError;
+use std::ops::Neg;
 use std::str::FromStr;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 /// Fee calculator input parameters for [VolatilityAdjustedFee]
 #[derive(Debug)]
 pub struct VolatilityAdjustedFee {
-    last_ewma_update_time: u64,
-    last_ewma_update_price: Decimal,
-    current_price: Decimal,
+    last_time: u64,
+    this_time: u64,
+    last_price: Decimal,
+    this_price: Decimal,
     ewma_window: u64,
-    ewma_var: Decimal,
+    last_ewma: Decimal,
     lambda: Decimal,
     velocity: Decimal,
     min_fee: Decimal,
     max_fee: Decimal,
 }
 
+#[derive(Default, Debug)]
+pub struct FeeResult {
+    pub last_time: u64,
+    pub last_price: u64,
+    pub last_ewma: u64,
+}
+
+impl Into<Vec<u64>> for FeeResult {
+    fn into(self) -> Vec<u64> {
+        vec![self.last_time, self.last_price, self.last_ewma]
+    }
+}
+
+impl From<Vec<u64>> for FeeResult {
+    fn from(vector: Vec<u64>) -> Self {
+        FeeResult {
+            last_time: vector[0],
+            last_price: vector[1],
+            last_ewma: vector[2],
+        }
+    }
+}
+
 impl Default for VolatilityAdjustedFee {
     fn default() -> Self {
         Self {
-            last_ewma_update_time: 0,
-            last_ewma_update_price: Decimal::from_u64(0).to_compute_scale(),
-            current_price: Decimal::from_u64(0).to_compute_scale(),
+            last_time: 0,
+            this_time: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("seconds")
+                .as_secs(),
+            last_price: Decimal::from_u64(0).to_compute_scale(),
+            this_price: Decimal::from_u64(0).to_compute_scale(),
             // 1h = 3600 seconds
             ewma_window: 3600,
-            // 1.25/(24/365)
-            ewma_var: Decimal::from_str("1.25").unwrap().to_compute_scale().div(
-                Decimal::from_u64(24)
-                    .to_compute_scale()
-                    .div(Decimal::from_u64(365).to_compute_scale()),
-            ),
+            // 1.25^2/365/24
+            last_ewma: Decimal::from_str("1.25")
+                .unwrap()
+                .to_compute_scale()
+                .pow(Decimal::two())
+                .div(Decimal::from_u64(24).to_compute_scale())
+                .div(Decimal::from_u64(365).to_compute_scale()),
             // 0.545
             lambda: Decimal::from_str("0.545").unwrap().to_compute_scale(),
             // 0.1 / 24 hours
@@ -47,94 +77,86 @@ impl Default for VolatilityAdjustedFee {
 }
 
 impl VolatilityAdjustedFee {
-    pub fn new(
-        last_ewma_update_time: u64,
-        last_ewma_update_price: Decimal,
-        current_price: Decimal,
-        ewma_window: u64,
-        ewma_var: Decimal,
-        lambda: Decimal,
-        velocity: Decimal,
-        min_fee: Decimal,
-        max_fee: Decimal,
-    ) -> Self {
-        Self {
-            last_ewma_update_time,
-            last_ewma_update_price,
-            current_price,
-            ewma_window,
-            ewma_var,
-            lambda,
-            velocity,
-            min_fee,
-            max_fee,
+    fn should_update(&self) -> bool {
+        self.last_time > 0
+            && self.this_time.checked_sub(self.last_time).unwrap() >= self.ewma_window
+    }
+
+    fn compute_ewma(&self) -> Decimal {
+        if self.should_update() {
+            // update
+            println!("DEBUG: Updating ewma");
+
+            // this_ewma = lambda * last_ewma + (1-lambda) * (this_price / last_price - 1)**2
+            // * ewma_window / (this_time - last_time)
+
+            // a = (1-lambda)
+            let a = Decimal::one().sub(self.lambda).unwrap();
+
+            // b = (this_price / last_price - 1)**2
+            let b = self
+                .this_price
+                .div(self.last_price)
+                .sub(Decimal::one())
+                .unwrap()
+                .pow(Decimal::two());
+
+            // c = ewma_window / (this_time - last_time)
+            let c = Decimal::from_u64(
+                self.ewma_window
+                    .checked_div(self.this_time.checked_sub(self.last_time).unwrap())
+                    .unwrap(),
+            )
+            .to_compute_scale();
+
+            self.lambda
+                .mul(self.last_ewma)
+                .add(a.mul(b).mul(c))
+                .unwrap()
+        } else {
+            self.last_ewma
         }
     }
 
     pub fn compute(
         &self,
         input_amount: &Decimal,
-    ) -> Result<(Decimal, Decimal), FeeCalculatorError> {
+    ) -> Result<(Decimal, Decimal, Decimal), FeeCalculatorError> {
         if self.min_fee.is_zero() || self.max_fee.is_zero() {
             return Ok((
                 Decimal::from_scaled_amount(0, input_amount.scale),
                 *input_amount,
+                self.last_ewma,
             ));
         }
 
-        let start = SystemTime::now();
-        let current_time = start.duration_since(UNIX_EPOCH).expect("seconds").as_secs();
-        if current_time
-            .checked_sub(self.last_ewma_update_time)
+        let ewma = self.compute_ewma();
+
+        // x = -ewma / 8
+        let x = ewma.neg().div(Decimal::from_u64(8).to_compute_scale());
+
+        // exp(x) = 1+x+x^2/2
+        let exp_x = Decimal::one()
+            .add(x)
             .unwrap()
-            > self.ewma_window
-        {
-            // update
-            println!("DEBUG: Updating");
-            let one = Decimal::from_u64(1).to_compute_scale();
-            let two = Decimal::from_u64(2).to_compute_scale();
+            .add(x.pow(2u128).div(Decimal::two()))
+            .unwrap();
 
-            // ewma_var = lambda * ewma_var + (1-lambda) * (current_price / last_ewma_update_price - 1)**2 * ewma_window / (current_time - Last_ewma_update_time)
+        let vol_adj_fee = Decimal::one().sub(exp_x).unwrap().div(self.velocity);
 
-            // a = (1-lambda)
-            let a = one.sub(self.lambda).unwrap();
+        // fee = MAX(min_fee,MIN(max_fee, vol_adj_fee)
+        let fee = self.min_fee.max(self.max_fee.min(vol_adj_fee));
 
-            // b = (current_price / last_ewma_update_price - 1)**2
-            let b = self
-                .current_price
-                .div(self.last_ewma_update_price)
-                .sub(one)
-                .unwrap()
-                .pow(two);
+        let amount_scaled = input_amount.to_compute_scale();
+        let fees = fee.mul(amount_scaled);
 
-            // c = ewma_window / (current_time - Last_ewma_update_time)
-            let c = Decimal::from_u64(
-                self.ewma_window
-                    .checked_div(
-                        current_time
-                            .checked_sub(self.last_ewma_update_time)
-                            .unwrap(),
-                    )
-                    .unwrap(),
-            )
-            .to_compute_scale();
-
-            let ewma_var = self.lambda.mul(self.ewma_var).add(a.mul(b).mul(c)).unwrap();
-        }
-
-        // fee = MAX(min_fee,MIN(max_fee, (1-math.exp(-ewma_var/8))/velocity))
-
-        let scaled_percentage = self.max_fee.to_scale(input_amount.scale);
-
-        let fees = input_amount.mul(scaled_percentage);
-
-        if fees.gte(*input_amount).unwrap() {
+        if fees.gte(amount_scaled).unwrap() {
             return Err(FeeCalculatorError::FeesGreaterThanAmount.into());
         }
 
         let amount_ex_fees = input_amount.sub(fees).expect("amount_ex_fees");
 
-        Ok((fees, amount_ex_fees))
+        Ok((fees, amount_ex_fees, ewma))
     }
 }
 
@@ -147,23 +169,18 @@ mod tests {
 
     #[test]
     fn test_compute_fees() {
-        let fee = VolatilityAdjustedFee::new(
-            0,
-            Decimal::from_u64(0).to_compute_scale(),
-            Decimal::from_u64(1).to_compute_scale(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-            Default::default(),
-        );
+        let fee = VolatilityAdjustedFee {
+            this_price: Decimal::from_scaled_amount(3400, 0).to_compute_scale(),
+            ..Default::default()
+        };
 
-        let (fees, amount_ex_fees) = fee
-            .compute(&Decimal::from_u64(1000).to_compute_scale())
+        let (fees, amount_ex_fees, ewma) = fee
+            .compute(&Decimal::from_scaled_amount(1000, 0).to_compute_scale())
             .unwrap();
 
-        println!("Fees: {:?}", fees);
-        println!("Amount ex fees: {:?}", amount_ex_fees);
+        println!("Fee: {:?}", fee);
+        println!("Fees: {:?}", fees.to_string());
+        println!("Amount ex fees: {:?}", amount_ex_fees.to_string());
+        println!("ewma: {:?}", ewma.to_string());
     }
 }
