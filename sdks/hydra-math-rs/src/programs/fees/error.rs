e107c3368cf103f925b2f1b9262687713861f0b6
WebDev717 <lasse.lin717@gmail.com>
2022-04-16 15:26:59 +0800

    reference fee calc and result

--- sdks/hydra-math-rs/src/programs/fees/error.rs
+++ sdks/hydra-math-rs/src/programs/fees/error.rs
@@ -1,9 +1,16 @@
+use crate::decimal::DecimalError;
+use crate::programs::fees::fee_result::FeeResultBuilderError;
 use thiserror::Error;
 
+/// Error codes related to [FeeCalculator].
 #[derive(Error, Debug)]
 pub enum FeeCalculatorError {
     #[error("Fees are greater than input amount")]
     FeesGreaterThanAmount,
     #[error("Failed to build struct due to input provided")]
     BuilderIncomplete,
+    #[error(transparent)]
+    DecimalError(#[from] DecimalError),
+    #[error(transparent)]
+    FeeResultBuilderError(#[from] FeeResultBuilderError),
 }
--- sdks/hydra-math-rs/src/programs/fees/mod.rs
+++ sdks/hydra-math-rs/src/programs/fees/mod.rs
@@ -1,3 +1,5 @@
 mod error;
+pub mod fee_calculator;
+pub mod fee_result;
 pub mod percentage_fee;
 pub mod volatility_adjusted_fee;
