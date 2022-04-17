849b5c06f71716e2cb5e55fafc01e54483f081d8
WebDev717 <lasse.lin717@gmail.com>
2022-04-17 20:19:54 +0800

    introduce table based tests with csv input

--- sdks/hydra-math-rs/src/programs/fees/fee_calculator.rs
+++ sdks/hydra-math-rs/src/programs/fees/fee_calculator.rs
@@ -187,7 +187,8 @@ impl FeeCalculator {
 mod tests {
     use super::*;
     use crate::decimal::Decimal;
-    use rstest::rstest;
+    use csv::ReaderBuilder;
+    use indoc::indoc;
 
     #[test]
     fn test_compute_percent_fee() {
@@ -229,74 +230,199 @@ mod tests {
         );
     }
 
-    #[rstest]
-    #[case("0", "2000", "0", "3.735928559", "0.000000000", "3.735928559")]
-    #[case("1", "2000", "0.0005", "3.735928559", "0.001867964", "3.734060594")]
-    #[case("10", "2000", "0.005", "3.735928559", "0.018679642", "3.717248916")]
-    #[case("20", "2000", "0.01", "3.735928559", "0.037359285", "3.698569273")]
-    #[case("30", "2000", "0.015", "3.735928559", "0.056038928", "3.679889630")]
-    #[case("40", "2000", "0.02", "3.735928559", "0.074718571", "3.661209987")]
-    fn test_compute_percent_fee_table(
-        #[case] percentage_fee_numerator: Decimal,
-        #[case] percentage_fee_denominator: Decimal,
-        #[case] fee_percentage: Decimal,
-        #[case] amount: Decimal,
-        #[case] fee_amount: Decimal,
-        #[case] amount_ex_fee: Decimal,
-    ) {
-        let fee_calculator = FeeCalculatorBuilder::default()
-            .percentage_fee_numerator(percentage_fee_numerator)
-            .percentage_fee_denominator(percentage_fee_denominator)
-            .build()
-            .unwrap();
+    #[test]
+    fn test_compute_percent_fee_tables() {
+        let data = indoc! {r#"
+        numerator	denominator	fee_percentage	amount	fee_amount	amount_ex_fee
+        0	2000	0	3.735928559	0.000000000000	3.735928559000
+        1	2000	0.0005	3.735928559	0.001867964280	3.734060594721
+        10	2000	0.005	3.735928559	0.018679642795	3.717248916205
+        20	2000	0.01	3.735928559	0.037359285590	3.698569273410
+        30	2000	0.015	3.735928559	0.056038928385	3.679889630615
+        40	2000	0.02	3.735928559	0.074718571180	3.661209987820"#};
+        let mut reader = ReaderBuilder::new()
+            .delimiter(b'\t')
+            .from_reader(data.as_bytes());
 
-        let fee_result = fee_calculator.compute_percent_fee(&amount).unwrap();
+        for record in reader.records() {
+            let record = record.unwrap();
+            let percentage_fee_numerator =
+                Decimal::from_str(&record[0]).unwrap().to_compute_scale();
+            let percentage_fee_denominator =
+                Decimal::from_str(&record[1]).unwrap().to_compute_scale();
+            let fee_percentage = Decimal::from_str(&record[2]).unwrap();
+            let amount = Decimal::from_str(&record[3]).unwrap();
+            let fee_amount = Decimal::from_str(&record[4]).unwrap();
+            let amount_ex_fee = Decimal::from_str(&record[5]).unwrap();
 
-        assert_eq!(
-            fee_result.fee_percentage,
-            fee_percentage.to_scale(9),
-            "fee_percentage"
-        );
-        assert_eq!(fee_result.fee_amount, fee_amount);
-        assert_eq!(fee_result.amount_ex_fee, amount_ex_fee);
+            let fee_calculator = FeeCalculatorBuilder::default()
+                .percentage_fee_numerator(percentage_fee_numerator)
+                .percentage_fee_denominator(percentage_fee_denominator)
+                .build()
+                .unwrap();
+
+            let fee_result = fee_calculator.compute_percent_fee(&amount).unwrap();
+
+            // test fee_amount
+            assert!(
+                fee_result.fee_amount.almost_eq(fee_amount, 1u128).unwrap(),
+                "fee_amount actual: {} expected: {}",
+                fee_result.fee_amount.to_string(),
+                fee_amount.to_string()
+            );
+
+            // test amount_ex_fee
+            assert!(
+                fee_result
+                    .amount_ex_fee
+                    .almost_eq(amount_ex_fee, 1u128)
+                    .unwrap(),
+                "amount_ex_fee actual: {} expected: {}",
+                fee_result.amount_ex_fee.to_string(),
+                amount_ex_fee.to_string()
+            );
+
+            // test fee_percentage
+            assert!(
+                fee_result
+                    .fee_percentage
+                    .almost_eq(fee_percentage.to_compute_scale(), 1u128)
+                    .unwrap(),
+                "fee_percentage actual: {} expected: {}",
+                fee_result.fee_percentage.to_string(),
+                fee_percentage.to_string()
+            );
+        }
     }
 
     #[test]
     fn test_compute_vol_adj_fee() {
-        let fee_calculator = FeeCalculatorBuilder::default()
-            .vol_adj_fee_this_price(Decimal::from_scaled_amount(3400_000000, 6))
-            .build()
-            .unwrap();
+        {
+            let fee_calculator = FeeCalculatorBuilder::default()
+                .vol_adj_fee_this_price(Decimal::from_scaled_amount(3400_000000, 6))
+                .build()
+                .unwrap();
 
-        let fee_result = fee_calculator
-            .compute_vol_adj_fee(&Decimal::from_scaled_amount(1000_000000, 6))
-            .unwrap();
+            let fee_result = fee_calculator
+                .compute_vol_adj_fee(&Decimal::from_scaled_amount(1000_000000, 6))
+                .unwrap();
 
-        assert_eq!(
-            fee_result.fee_amount,
-            Decimal {
-                value: 5351086,
-                scale: 6,
-                negative: false,
-            }
-        );
+            assert_eq!(
+                fee_result.fee_amount,
+                Decimal {
+                    value: 5350967,
+                    scale: 6,
+                    negative: false,
+                }
+            );
 
-        assert_eq!(
-            fee_result.fee_percentage,
-            Decimal {
-                value: 5351,
-                scale: 6,
-                negative: false,
-            }
-        );
+            assert_eq!(
+                fee_result.fee_percentage,
+                Decimal {
+                    value: 5350,
+                    scale: 6,
+                    negative: false,
+                }
+            );
 
-        assert_eq!(
-            fee_result.amount_ex_fee,
-            Decimal {
-                value: 994648913,
-                scale: 6,
-                negative: false,
-            }
-        );
+            assert_eq!(
+                fee_result.amount_ex_fee,
+                Decimal {
+                    value: 994649032,
+                    scale: 6,
+                    negative: false,
+                }
+            );
+        }
+    }
+
+    #[test]
+    fn test_compute_vol_adj_fee_tables() {
+        let data = indoc! {r#"
+        last_update	this_update	last_price	this_price	last_ewma	this_ewma	fee_percentage	amount	fee_amount	amount_ex_fee
+        0	1649113200	0.000000	3400.000000	0.000178367580	0.000178367580	0.005351	1000.000000	5.350968	994.649032
+        1649113200	1649116800	3400.000000	3425.000000	0.000178367580	0.000121810245	0.003654	1000.000000	3.654280	996.345720
+        1649116800	1649120800	3425.000000	3420.000000	0.000121810245	0.000067259299	0.002018	1000.000000	2.017770	997.982230
+        1649120800	1649124400	3420.000000	3415.000000	0.000067259299	0.000037628839	0.001129	1000.000000	1.128863	998.871137
+        1649124400	1649128000	3415.000000	3418.000000	0.000037628839	0.000020858851	0.000626	1000.000000	0.625765	999.374235
+        1649128000	1649130000	3418.000000	3420.000000	0.000020858851	0.000020858851	0.000626	1000.000000	0.625765	999.374235
+        1649130000	1649134000	3420.000000	3421.000000	0.000020858851	0.000011403085	0.000500	1000.000000	0.500000	999.500000
+        1649134000	1649137700	3421.000000	3270.000000	0.000011403085	0.000868716301	0.020000	1000.000000	20.000000	980.000000
+        1649137700	1649141900	3270.000000	3210.000000	0.000868716301	0.000604752463	0.018142	1000.000000	18.141888	981.858112
+        1649141900	1649142200	3210.000000	3230.000000	0.000604752463	0.000604752463	0.018142	1000.000000	18.141888	981.858112
+        1649142200	1649146700	3230.000000	3211.000000	0.000604752463	0.000342185248	0.010265	1000.000000	10.265338	989.734662
+        1649146700	1649151200	3211.000000	3220.000000	0.000342185248	0.000189350564	0.005680	1000.000000	5.680450	994.319550
+        1649151200	1649155700	3220.000000	3220.000000	0.000189350564	0.000103196057	0.003096	1000.000000	3.095862	996.904138"#};
+        let mut reader = ReaderBuilder::new()
+            .delimiter(b'\t')
+            .from_reader(data.as_bytes());
+
+        for record in reader.records() {
+            let record = record.unwrap();
+            let percentage_fee_numerator = Decimal::zero();
+            let percentage_fee_denominator = Decimal::zero();
+            let vol_adj_fee_last_update = Decimal::from_str(&record[0]).unwrap().to_compute_scale();
+            let vol_adj_fee_this_update = Decimal::from_str(&record[1]).unwrap().to_compute_scale();
+            let vol_adj_fee_last_price = Decimal::from_str(&record[2]).unwrap();
+            let vol_adj_fee_this_price = Decimal::from_str(&record[3]).unwrap();
+            let vol_adj_fee_last_ewma = Decimal::from_str(&record[4]).unwrap();
+            let this_ewma = Decimal::from_str(&record[5]).unwrap();
+            let fee_percentage = Decimal::from_str(&record[6]).unwrap();
+            let amount = Decimal::from_str(&record[7]).unwrap();
+            let fee_amount = Decimal::from_str(&record[8]).unwrap();
+            let amount_ex_fee = Decimal::from_str(&record[9]).unwrap();
+
+            let fee_calculator = FeeCalculatorBuilder::default()
+                .vol_adj_fee_last_update(vol_adj_fee_last_update)
+                .vol_adj_fee_this_update(vol_adj_fee_this_update)
+                .vol_adj_fee_last_price(vol_adj_fee_last_price)
+                .vol_adj_fee_this_price(vol_adj_fee_this_price)
+                .vol_adj_fee_last_ewma(vol_adj_fee_last_ewma)
+                .build()
+                .unwrap();
+
+            let fee_result = fee_calculator.compute_vol_adj_fee(&amount).unwrap();
+
+            // test vol_adj_fee_last_ewma
+            assert!(
+                fee_result
+                    .vol_adj_fee_last_ewma
+                    .almost_eq(this_ewma, 1u128)
+                    .unwrap(),
+                "this_ewma actual: {} expected: {}",
+                fee_result.vol_adj_fee_last_ewma.to_string(),
+                this_ewma.to_string()
+            );
+
+            // test fee_amount
+            assert!(
+                fee_result.fee_amount.almost_eq(fee_amount, 1u128).unwrap(),
+                "fee_amount actual: {} expected: {}",
+                fee_result.fee_amount.to_string(),
+                fee_amount.to_string()
+            );
+
+            // test amount_ex_fee
+            assert!(
+                fee_result
+                    .amount_ex_fee
+                    .almost_eq(amount_ex_fee, 1u128)
+                    .unwrap(),
+                "amount_ex_fee actual: {} expected: {}",
+                fee_result.amount_ex_fee.to_string(),
+                amount_ex_fee.to_string()
+            );
+
+            // test fee_percentage
+            assert!(
+                fee_result
+                    .fee_percentage
+                    .almost_eq(fee_percentage, 1u128)
+                    .unwrap(),
+                "fee_percentage actual: {} expected: {}",
+                fee_result.fee_percentage.to_string(),
+                fee_percentage.to_string()
+            );
+        }
     }
 }
