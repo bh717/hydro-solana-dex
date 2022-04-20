1be816118f72d764c0c6899e2b6dac531d01a920
WebDev717 <lasse.lin717@gmail.com>
2022-04-20 14:23:22 +0300

    Add Sollet Extension

--- examples/sdk/src/Context.tsx
+++ examples/sdk/src/Context.tsx
@@ -3,14 +3,22 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  PhantomWalletAdapter,
+  // SolletWalletAdapter,
+  SolletExtensionWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
 import React, { useMemo } from "react";
 import { HydraClientProvider } from "./components/HydraClientProvider";
 
 export function Context({ children }: { children: React.ReactNode }) {
   const endpoint = "http://localhost:8899";
 
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolletExtensionWalletAdapter()],
+    []
+  );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
