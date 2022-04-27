4394f8afb4c409d5f928e8a3b705d33035f7d59e
WebDev717 <lasse.lin717@gmail.com>
2022-04-27 10:18:00 +0300

    Remove HydraProvider

--- sdks/hydra-react-ts/src/index.ts
+++ sdks/hydra-react-ts/src/index.ts
@@ -11,4 +11,10 @@ export * from "./hooks/useToken";
 export * from "./hooks/useAssetBalances";
 export * from "./hooks/usePools";
 export * from "./WalletProvider";
+
+// TODO: For some reason within our monorepo not pulling this
+//       package from here causes incompatabilities when attempting
+//       to use it within the sdk example app. This stuff is only used for
+//       the buttons within the exampole interface. Example app is likely
+//       temporary anyway
 export * from "@solana/wallet-adapter-react-ui";
