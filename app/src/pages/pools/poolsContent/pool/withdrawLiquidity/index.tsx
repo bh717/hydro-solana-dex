c93db45b8b75a64b7fbfc5ab2c904e4bee3a98dc
WebDev717 <lasse.lin717@gmail.com>
2022-05-01 21:02:56 -0500

    Integrate Remove Liquidity - Withdraw

--- app/src/pages/pools/poolsContent/pool/withdrawLiquidity/index.tsx
+++ app/src/pages/pools/poolsContent/pool/withdrawLiquidity/index.tsx
@@ -18,7 +18,7 @@ const WithdrawLiquidityModal: FC<WithdrawLiquidityModalProps> = ({
   tokenB,
 }) => (
   <Modal
-    content={<Content tokenA={tokenA} tokenB={tokenB} />}
+    content={<Content tokenAInit={tokenA} tokenBInit={tokenB} />}
     open={open}
     onClose={onClose}
   />
